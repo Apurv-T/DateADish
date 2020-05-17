8be5ec3d94a5941d6eb12b8488e237f7e30641f2
Apurv Tripathi <ap364626@dal.ca>
2020-05-17 18:38:53 -0300

    Array used for fast appliaction load.

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,32 @@ void main() {
         title: Text('Date A Dish'),
         backgroundColor: Colors.red[200],
       ),
-      body: dish(),
+      body: Dish(),
     ),
   ));
 }
 
-class dish extends StatefulWidget {
+class Dish extends StatefulWidget {
   @override
-  _dishState createState() => _dishState();
+  _DishState createState() => _DishState();
 }
 
-class _dishState extends State<dish> {
-  int newImage = 1;
+class _DishState extends State<Dish> {
+  dynamic newImage =
+      'https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80';
   @override
   Widget build(BuildContext context) {
     void randomFood() {
       setState(() {
-        newImage = Random().nextInt(6) + 1;
+        var images = [
+          'https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80',
+          'https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+          'https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+          'https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+          'https://images.unsplash.com/photo-1506084868230-bb9d95c24759?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+          'https://images.unsplash.com/photo-1588166524941-3bf61a9c41db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        ];
+        newImage = images.elementAt(Random().nextInt(6) + 1);
       });
     }
 
@@ -37,7 +46,19 @@ class _dishState extends State<dish> {
             Image(
               height: 450.0,
               width: 300.0,
-              image: AssetImage('images/food$newImage.jpg'),
+              image: NetworkImage('$newImage'),
+              loadingBuilder: (BuildContext context, Widget child,
+                  ImageChunkEvent loadingProgress) {
+                if (loadingProgress == null) return child;
+                return Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                            loadingProgress.expectedTotalBytes
+                        : null,
+                  ),
+                );
+              },
             ),
             SizedBox(
               height: 20.0,
